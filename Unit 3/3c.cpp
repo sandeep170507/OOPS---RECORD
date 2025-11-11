28fbbff0e0d6dced0957a4b94425f4dc217250fe
sandeep170507 <11249a041@kanchiuniv.ac.in>
2025-11-11 03:16:42 -0800

    Add files via upload

---
+++ Unit 3/3c.cpp
@@ -0,0 +1,22 @@
+//unary operator overloading
+#include <iostream>
+using namespace std;
+class Temperature {
+float celsius;
+public:
+void getData() {
+cout << "Enter temperature in Celsius: ";
+cin >> celsius;
+}
+void operator-() {
+float fahrenheit = (celsius * 9/5) + 32;
+cout << "Temperature in Fahrenheit: " << fahrenheit << endl;
+}
+};
+int main() {
+Temperature t;
+t.getData();
+-t;
+return 0;
+
+}
